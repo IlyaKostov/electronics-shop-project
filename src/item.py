b607a488058808b3fa21fc9fa6593625b2710f68
Kostov Ilya <kick1991@mail.ru>
2023-06-23 00:09:11 +0500

    add tests, add instantiate_from_csv, add string_to_number, add property and private name

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +16,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
@@ -33,3 +36,29 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            self.__name = name[:10]
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        cls.all = []
+        with open('../src/items.csv', encoding='windows-1251') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                name = row['name']
+                price = cls.string_to_number(row['price'])
+                quantity = cls.string_to_number(row['quantity'])
+                cls(name, price, quantity)
+
+    @staticmethod
+    def string_to_number(num: str):
+        return int(float(num))
--- tests/test_item.py
+++ tests/test_item.py
@@ -6,17 +6,42 @@ from src.item import Item
 
 @pytest.fixture
 def item():
-    return Item("Мышь", 100, 20)
+    item1 = Item("Мышь", 100, 20)
+    return item1
+
+
+def test_init(item):
+    assert item.name == 'Мышь'
+    assert item.price == 100
+    assert item.quantity == 20
 
 
 def test_item_total_price(item):
-    '''Проверяем правильность расчета общей стоимости товара'''
+    """Проверяем правильность расчета общей стоимости товара"""
     assert item.calculate_total_price() == 2000
 
 
 def test_item_apply_discount(item):
-    '''Проверяем расчет скидки'''
+    """Проверяем расчет скидки"""
     item.pay_rate = 0.8
     item.apply_discount()
     assert 100 * item.pay_rate == item.price
     assert item.price == 80
+
+
+def test_item_instantiate_from_csv():
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
+
+
+def test_item_string_to_number():
+    assert Item.string_to_number('10') == 10
+    assert Item.string_to_number('15.0') == 15
+    assert Item.string_to_number('25.5') == 25
+
+
+def test_name_setter(item):
+    item.name = 'Мышка'
+    assert item.name == 'Мышка'
+    item.name = 'Суперпупермышь'
+    assert item.name == 'Суперпупер'
